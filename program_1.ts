5290e9b25bcca673337f2045f46b2acc5bddfd94
Suzana-dua <161020777+Suzana-dua@users.noreply.github.com>
2024-02-22 22:53:09 -1200

    Add files via upload

---
+++ program_1.ts
@@ -0,0 +1,4 @@
+//Program written by Suzana
+//Current Date :22-2-2024
+//This program will write Hello World
+console.log('Hello,World!');
---
+++ program_2.ts
@@ -0,0 +1,4 @@
+//Program written by Suzana
+//Current Date :22-2-2024
+//This program will add two digits
+console.log(2+2);
